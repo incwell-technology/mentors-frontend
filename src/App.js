a182ec25c565be9c75b5cef720f85e4378153839
Prashant-Acharya <pacharya97.pa@gmail.com>
2019-05-22 15:43:05 +0545

    Login Feature Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router , Route } from 'react-router-dom'
+import './App.css'
+
 import Authentication from './components/Authentications/Authentication';
+import Home from './components/Home'
+
 
 function App() {
     return (
         <>
-            <Authentication />
+            <Router>
+                <Route exact path='/' component={Authentication} />
+                <Route path='/home' component={Home} />
+            </Router>
         </>
     );
 }
--- src/components/Authentications/Login.jsx
+++ src/components/Authentications/Login.jsx
@@ -1,36 +1,59 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
-export default class Login extends Component {
-    render() {
-        return (
-            <>
-            <form onSubmit={this.handleSubmit} method="post">
-                <input type="email" onChange={this.handleInput} name="email" placeholder="Email address" />
-                <input type="password" onChange={this.handleInput} name="password" placeholder="Password" />
-                <input type="submit" value="Login" />
-            </form>
-            <div className="signup">
-                <p>or</p>
-                <ul>
-                    <p>Sign in with</p>
-                    <li>
-                        <a href="#">
-                            <i className="fab fa-facebook" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i className="fab fa-google" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i className="fab fa-linkedin-in" />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </>
-        )
-    }
+class Login extends Component {
+	state = {
+		email: '',
+		password: '',
+		status: ''
+	}
+
+	handleSubmit= async e => {
+		e.preventDefault()
+		const res = await axios.post('http://192.168.1.125:3000/mentors/login', this.state)
+		console.log(res)
+		this.setState({status: res.status})
+	}
+
+	handleInput = e => {
+		this.setState({ [e.target.name]: e.target.value })	
+	}
+
+	render() {
+		return (
+			<>
+				{this.state.status && <Redirect to='/home' />}
+				<form onSubmit={this.handleSubmit} method="post">
+					<input type="email" onChange={this.handleInput} name="email" placeholder="Email address" />
+					<input type="password" onChange={this.handleInput} name="password" placeholder="Password" />
+					<input type="submit" value="Login" />
+				</form>
+				<div className="signup">
+					<p>or</p>
+					<ul>
+						<p>Sign in with</p>
+						<li>
+							<a href="#">
+								<i className="fab fa-facebook" />
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i className="fab fa-google" />
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i className="fab fa-linkedin-in" />
+							</a>
+						</li>
+					</ul>
+				</div>
+			</>
+		)
+	}
 }
+
+
+export default Login

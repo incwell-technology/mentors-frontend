75fb784d46748440c3ee59369d089c58531e609d
Prashant Acharya <prashant.acharya@sagarmatha.edu.np>
2019-05-30 11:05:09 +0545

    feat: Error handling alert added.

--- src/components/authentications/social/google/GoogleLogin.jsx
+++ src/components/authentications/social/google/GoogleLogin.jsx
@@ -2,38 +2,38 @@ import React, { Component } from 'react'
 import { GoogleLogin } from 'react-google-login'
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
+import swal from 'sweetalert'
 
 class GoogleLoginComponent extends Component {
 	state = {
-		status: ''
+		success: ''
 	}
 
 	handleAuth = async (res) => {
 		try {
-			console.log(res.accessToken)
-			const data = await axios.post('http://192.168.1.130:3000/v1/auth/google', {accessToken: res.accessToken})
-			console.log(data)
-			this.setState({status: data.status})
+			const data = await axios.post('http://192.168.1.130:3000/v1/auth/google', { accessToken: res.accessToken })
+			this.setState({ success: data.data.success })
 		} catch (error) {
-			console.log(error)
+			this.setState({ success: 'false' })
 		}
 	}
 
 	render() {
 		return (
 			<li>
-				{ this.state.status === 200 && <Redirect push to='home' /> }
+				{this.state.success === 'true' && <Redirect push to='home' />}
+				{this.state.success === 'false' && swal("Sign in failed. Please try again.")}
 				<GoogleLogin
 					clientId="442918571972-gkidqu670g878nl2d889hlj11gei0068.apps.googleusercontent.com"
 					render={renderProps => (
-						<i style={{cursor: 'pointer'}} className="fab fa-google" onClick={renderProps.onClick} disabled={renderProps.disabled}></i>
-					  )}
+						<i style={{ cursor: 'pointer' }} className="fab fa-google" onClick={renderProps.onClick} disabled={renderProps.disabled}></i>
+					)}
 					buttonText="Login"
 					onSuccess={this.handleAuth}
 					onFailure={this.handleAuth}
 					cookiePolicy={'single_host_origin'}
 				/>
-					{/* <a href="#">
+				{/* <a href="#">
 						<i className="fab fa-google" />
 					</a> */}
 

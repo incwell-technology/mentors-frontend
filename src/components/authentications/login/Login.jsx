33119a23e214740bc3751e6c601b395a5ff2f9cd
Prashant Acharya <prashant.acharya@sagarmatha.edu.np>
2019-06-11 14:23:14 +0545

    fix

--- src/components/authentications/login/Login.jsx
+++ src/components/authentications/login/Login.jsx
@@ -3,7 +3,6 @@ import axios from 'axios'
 import { Redirect } from 'react-router-dom'
 import Social from '../social/Social'
 
-
 let base_url
 if (process.env.NODE_ENV === "development") {
 	base_url = process.env.REACT_APP_DEV_URL +'/v1/mentors/login'
@@ -23,7 +22,8 @@ class Login extends Component {
 	state = {
 		email: '',
 		password: '',
-		status: ''
+		status: '',
+		error: false
 	}
 
 	handleSubmit = async e => {
@@ -34,7 +34,7 @@ class Login extends Component {
 				this.setState({ status: res.status })
 			}
 		} catch (error) {
-			console.log(error)
+			this.setState({error: true})
 		}
 	}
 
@@ -47,6 +47,7 @@ class Login extends Component {
 			<>
 				{this.state.status === 200 && <Redirect push to='/page' />}
 				{this.state.status === 409 && <Failed />}
+				{this.state.error && <div className='warning'>Login Failed</div>}
 				<form onSubmit={this.handleSubmit} method="post">
 					<input type="email" onChange={this.handleInput} name="email" placeholder="Email address" />
 					<input type="password" onChange={this.handleInput} name="password" placeholder="Password" />
